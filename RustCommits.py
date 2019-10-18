cf673c3b47fff6a695658c822917d77a380a37d0
Tudor Velican <tudorvelican@gmail.com>
2019-10-19 01:14:15 +0200

    Added some functions

--- RustCommits.py
+++ RustCommits.py
@@ -2,7 +2,14 @@ import requests
 import bs4
 
 #TODO twitter API authentication
+def main():
+    pass
 
+def authentication():
+    pass
 
-
+def sync_tweets():
+    pass
 #TODO setup tweet stream using some API  
+
+#TODO  make using classes?
